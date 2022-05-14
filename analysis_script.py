fea3076ba778683f53d046b3e04d01b9d6401a48
Dawreen <dntknwhtf@gmail.com>
2022-05-14 10:12:06 +0200

    using .loc[] update script

--- analysis_script.py
+++ analysis_script.py
@@ -19,7 +19,7 @@ reg_price_filepath = filepath + "/datasets/registered-prices-apartements-by-comm
 
 # %% [markdown]
 # We open the dataset and start working on the indexing, organizing the data by Quarter and Year.
-# Since the 3 datasets are similar we can create a function to avoid repeating code.
+# Since the first 3 datasets are similar we can create a function to avoid repeating code.
 
 # %%
 def clean_index(dataset):
@@ -37,11 +37,16 @@ def clean_index(dataset):
 
     dataset.sort_index(inplace=True)
 
+    # we don't want to waste any data so we will return these info
+    ret = [dataset.loc['Luxembourg City'], dataset.loc['National Average']]
+
     dataset.drop(index='Luxembourg City', inplace=True)
     dataset.drop(index='National Average', inplace=True)
 
+    return ret
+
 # %% [markdown]
-# We start checking the datatype and we start handling the NaN.
+# After we cleaned the data we check the datatype and we handle the missing that.
 
 # %%
 def check_type_missing(dataset, missing, rent=None):
@@ -56,27 +61,38 @@ def check_type_missing(dataset, missing, rent=None):
     # easy handling of missing data, may change for better modeling
     val = missing
     dataset.replace('*', val, inplace=True)
-    dataset[col1] = dataset[col1].astype('int64')
-    dataset[col2] = dataset[col2].astype('float64').round(2)
-    dataset[col3] = dataset[col3].astype('float64').round(2)
+    dataset.loc[:,col1] = dataset.loc[:,col1].astype('int64')
+    dataset.loc[:,col2] = dataset.loc[:,col2].astype('float64').round(2)
+    dataset.loc[:,col3] = dataset.loc[:,col3].astype('float64').round(2)
 
     # type check
     print(f"{col1 + ':':<50} \
-        {str(dataset[col1].dtype)}")
+        {str(dataset.loc[:,col1].dtype)}")
     print(f"{col2 + ':':<50} \
-        {str(dataset[col2].dtype)}")
+        {str(dataset.loc[:,col2].dtype)}")
     print(f"{col3 + ':':<50} \
-        {str(dataset[col3].dtype)}")
+        {str(dataset.loc[:,col3].dtype)}")
+
+# %% [markdown]
+# We aply the functions we've defined on the first 3 datasets.
 
 # %%
 # acquiring the data
 price_ap_data = pd.read_excel(price_app_filepath)
 
-# cleaning the indexing
-clean_index(price_ap_data)
-
 price_ap_data
 
+# %%
+
+# cleaning the indexing
+temp =  clean_index(price_ap_data)
+d1_lux_avg = temp[0]
+d1_nat_avg = temp[1]
+
+print(d1_lux_avg)
+print(d1_nat_avg)
+
+price_ap_data
 
 # %%
 # checking the types and handling missing values
@@ -87,8 +103,14 @@ price_ap_data
 # acquiring the data
 price_hous_data = pd.read_excel(price_house_filepath)
 
-# clening the indexing
-clean_index(price_hous_data)
+# cleaning the indexing
+temp = clean_index(price_hous_data)
+
+d2_lux_avg = temp[0]
+d2_nat_avg = temp[1]
+
+print(d2_lux_avg)
+print(d2_nat_avg)
 
 price_hous_data.head()
 
@@ -102,7 +124,13 @@ price_hous_data
 rent_ap_data = pd.read_excel(rent_ap_filepath)
 
 # cleaning the indexing
-clean_index(rent_ap_data)
+temp = clean_index(rent_ap_data)
+
+d3_lux_avg = temp[0]
+d3_nat_avg = temp[1]
+
+print(d3_lux_avg)
+print(d3_nat_avg)
 
 rent_ap_data.head()
 
@@ -116,23 +144,24 @@ reg_price_data
 
 # %%
 tuples1 = []
-for el in reg_price_data.iloc[0][1:4]:
+for el in reg_price_data.iloc[0, 1:4]:
     tuples1.append(("Constructed", el))
-for el in reg_price_data.iloc[0][1:4]:
+for el in reg_price_data.iloc[0, 1:4]:
     tuples1.append(("VEFA", el))
 
 print(tuples1)
 
 new_header = reg_price_data.iloc[0]
 reg_price_data.columns = new_header
-reg_price_data = reg_price_data[1:]
+reg_price_data = reg_price_data.iloc[1:]
 
 reg_price_data
 
 # %%
-# ordering the data by quarter and year, creating a multi-index
-arrays = [[*reg_price_data['Commune']], [*reg_price_data.Year]]
+# ordering the data by Commune and year, creating a multi-index
+arrays = [[*reg_price_data.loc[:,'Commune']], [*reg_price_data.loc[:,'Year']]]
 tuples = list(zip(*arrays))
+
 index =pd.MultiIndex.from_tuples(tuples, names=['Commune', 'Year'])
 reg_price_data.set_index(index, inplace=True)
 
@@ -140,16 +169,23 @@ reg_price_data.set_index(index, inplace=True)
 reg_price_data.drop(columns=['Commune', 'Year'], inplace=True)
 
 reg_price_data.sort_index(inplace=True)
+
+d4_nat_avg = reg_price_data.loc['National Average']
+# print(d4_nat_avg)
+
 reg_price_data.drop(index='National Average', inplace=True)
 
-index = pd.MultiIndex.from_tuples(tuples1, names=["Construction State", "Detail"])
-reg_price_data.columns = index
+new_columns = pd.MultiIndex.from_tuples(tuples1, names=["Construction State", "Detail"])
+reg_price_data.columns = new_columns
 
 reg_price_data
 
 # %%
-reg_price_data.loc[:]["Constructed", "Price range for price per squared meter"]
+reg_price_data.loc[:,("Constructed", "Price range for price per squared meter")]
 # TODO #2 parse the range constructed
 # TODO #3 parse the range VEFA
+
+# %%
+
 
 
